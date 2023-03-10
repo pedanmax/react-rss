c3392c55f9c992f838b7a2de67939518f3e03911
pedanmax <m.pedan2017@gmail.com>
2023-03-10 16:34:01 +0200

    init:install all plugins for development

---
+++ react/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+  return <div className='App'>Hello world</div>;
+}
+
+export default App;
---
+++ react/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint(), checker({ typescript: true })],
+});
