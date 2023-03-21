0b55f1aaf0cee5417dc9cb113f321840dfcb82d5
pedanmax <m.pedan2017@gmail.com>
2023-03-21 09:59:06 +0200

    feat:create form page & add form to navigation

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,6 +5,7 @@ import About from './pages/About/About';
 import NotFound from './pages/NotFound/NotFound';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Form from './pages/Form/Form';
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/about' element={<About />}></Route>
+          <Route path='/form' element={<Form />}></Route>
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </div>
---
+++ react/src/pages/Form/Form.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Form.scss';
+
+class Form extends React.Component {
+  render() {
+    return <h1 className='form__title form'>Form page</h1>;
+  }
+}
+
+export default Form;
