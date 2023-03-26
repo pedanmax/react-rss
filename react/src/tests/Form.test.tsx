9b929ad58df93649fdf435f8845d19ca4d0f6240
pedanmax <m.pedan2017@gmail.com>
2023-03-26 11:12:09 +0300

    refactor:check all files by prettier

--- react/src/tests/Form.test.tsx
+++ react/src/tests/Form.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Form from '../components/Form/Form';
 import { vi } from 'vitest';
 
