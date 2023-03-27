a322157ec398d2244977c3414c24221ad1f27747
pedanmax <m.pedan2017@gmail.com>
2023-03-27 20:34:43 +0300

    refactr:change app file to functional component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -7,20 +7,18 @@ import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Form from './pages/FormPage/FormPage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/form' element={<Form />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/form' element={<Form />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
