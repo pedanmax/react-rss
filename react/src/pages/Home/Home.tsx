575cf989937fd0754cbb57c0589bbf296b66120c
pedanmax <m.pedan2017@gmail.com>
2023-03-28 09:55:34 +0300

    feat: change Home to functional component

--- react/src/pages/Home/Home.tsx
+++ react/src/pages/Home/Home.tsx
@@ -3,19 +3,17 @@ import Search from '../../components/Search/Search';
 import CardList from '../../components/CardList/CardList';
 import './Home.scss';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className='home'>
-        <div className='home__container'>
-          <div className='home__search'>
-            <Search />
-          </div>
-          <CardList />
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <div className='home__search'>
+          <Search />
         </div>
+        <CardList />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
